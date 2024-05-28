eb1e6ef3ed551a0a8ed58f503a84d26471e09632
Elvin Ega <proega3@gmail.com>
2024-05-28 19:46:26 +0300

    add api route

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import SocketProvider from "@/context/SocketProvider";
+
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return <SocketProvider>{children} </SocketProvider>;
 }
